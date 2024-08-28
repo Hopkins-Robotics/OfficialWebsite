70fb4644f7415f1cd0bcf78c77a03f70ecbc7295
Michael <illuzzimichael99@gmail.com>
2024-08-28 13:38:28 -0400

    a bit of work on mailing list

--- mailinglist.php
+++ mailinglist.php
@@ -6,11 +6,22 @@ $dbname = "emails";
 $username = "root";
 $password = "";
 
-$sconn = mysqli_connect($host, $username, $password, $dbname);
+$conn = mysqli_connect($host, $username, $password, $dbname);
 if (mysqli_connect_errno()) {
     die("Connection error". mysqli_connect_error());
 }
+try {
+    $sql = "INSERT INTO emails (email) 
+        VALUES (?);";
 
-$sql = "INSERT INTO emails (body)
-        VALUES ('$email')"
+    $stmt = mysqli_stmt_init($conn);
+    if( ! mysqli_stmt_prepare($stmt,$sql)) {
+        die(mysqli_error($conn));
+    }
+    mysqli_stmt_bind_param($stmt, "s", $email);
+    echo "record saved";
+
+} catch (PDOExceptopm $e) {
+    die("Submit error" . $e->getMessage());
+}
 ?>
