574e48f49ece25b0ff81597bc028e722684b67b3
Michael <illuzzimichael99@gmail.com>
2024-08-28 01:05:50 -0400

    php work

--- mailinglist.php
+++ mailinglist.php
@@ -1,3 +1,16 @@
 <?php
-    print_r($_POST)
+$email = $_POST["email"];
+
+$host = "localhost";
+$dbname = "emails";
+$username = "root";
+$password = "";
+
+$sconn = mysqli_connect($host, $username, $password, $dbname);
+if (mysqli_connect_errno()) {
+    die("Connection error". mysqli_connect_error());
+}
+
+$sql = "INSERT INTO emails (body)
+        VALUES ('$email')"
 ?>
