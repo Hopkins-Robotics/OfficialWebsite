c6d79012fd5273603e8f26e7d57fe3189189cecf
Michael <illuzzimichael99@gmail.com>
2024-08-29 21:49:51 -0400

    php praying

--- mailinglist.php
+++ mailinglist.php
@@ -1,27 +1,38 @@
 <?php
 $email = $_POST["email"];
 
-$host = "localhost";
-$dbname = "emails";
+$host = "localhost:3306";
+$dbname = "juzxkxdv_mailinglist";
 $username = "root";
 $password = "";
 
 $conn = mysqli_connect($host, $username, $password, $dbname);
 if (mysqli_connect_errno()) {
-    die("Connection error". mysqli_connect_error());
+    die("Connection error: " . mysqli_connect_error());
 }
+
 try {
-    $sql = "INSERT INTO emails (email) 
-        VALUES (?);";
+    $sql = "INSERT INTO emails (email) VALUES (?);";
 
     $stmt = mysqli_stmt_init($conn);
-    if( ! mysqli_stmt_prepare($stmt,$sql)) {
-        die(mysqli_error($conn));
+    if (!mysqli_stmt_prepare($stmt, $sql)) {
+        die("SQL error: " . mysqli_error($conn));
     }
-    mysqli_stmt_bind_param($stmt, "s", $email);
-    echo "record saved";
 
-} catch (PDOExceptopm $e) {
-    die("Submit error" . $e->getMessage());
+    mysqli_stmt_bind_param($stmt, "s", $email);
+
+    // test stuff to see if it works, added extra context
+    if (mysqli_stmt_execute($stmt)) {
+        echo "Record saved";
+    } else {
+        die("Execute error: " . mysqli_stmt_error($stmt));
+    }
+
+    // close connection
+    mysqli_stmt_close($stmt);
+    mysqli_close($conn);
+
+} catch (Exception $e) {
+    die("Submit error: " . $e->getMessage());
 }
 ?>
